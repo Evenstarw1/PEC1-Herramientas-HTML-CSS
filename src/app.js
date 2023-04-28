8dd7141a079661eb05f626b317cd0dac570f52a6
Laura Corral <laura.corral.contractor@bbva.com>
2023-04-28 22:04:58 +0200

    feat(node_modules): Added node modules folder

--- src/app.js
+++
@@ -1,9 +0,0 @@
-const userName = prompt('Hola, ¿cuál es tu nombre?', '')
-
-if (userName != null) {
-  document.getElementById('welcomeMessage').innerHTML =
-    'Hola ' + userName + '&#9829;'
-} else {
-  document.getElementById('welcomeMessage').innerHTML =
-    'Hola desconocido &#9829;'
-}
